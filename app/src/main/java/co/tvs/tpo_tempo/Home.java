1c8aea90e00dccc0d1a13db445b47b1101823ba1
Vivek19915 <vchoudhary19915@gmail.com>
2022-03-13 23:16:26 +0530

    splash screen added

---
+++ app/src/main/java/co/tvs/tpo_tempo/Home.java
@@ -0,0 +1,14 @@
+package co.tvs.tpo_tempo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
--- app/src/main/java/co/tvs/tpo_tempo/MainActivity.java
+++ app/src/main/java/co/tvs/tpo_tempo/MainActivity.java
@@ -2,13 +2,51 @@ package co.tvs.tpo_tempo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.drawable.Animatable;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.lang.reflect.GenericArrayType;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static int SPLASH_SCREEN = 3000;
+
+    //variables
+    Animation top_animation,bottom_animation;
+    ImageView image;
+    TextView logo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
+        //animations calling
+        top_animation = AnimationUtils.loadAnimation(this,R.anim.top_animation);
+        bottom_animation = AnimationUtils.loadAnimation(this,R.anim.bottom_animation);
+
+        //hooks
+        image = findViewById(R.id.imageView2);
+        logo = findViewById(R.id.textView);
+
+        image.setAnimation(bottom_animation);
+        logo.setAnimation(top_animation);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent  = new Intent(MainActivity.this,Home.class);
+                startActivity(intent);
+                finish();
+            }
+        },SPLASH_SCREEN);
     }
 }
